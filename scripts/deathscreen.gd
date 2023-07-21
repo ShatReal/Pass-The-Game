6a1141c0c536b2d8c801d5fd5ed15a36305fb56f
Alexander <115094566+Agamess@users.noreply.github.com>
2023-07-21 11:04:28 +0200

    Delete scripts directory

--- scripts/deathscreen.gd
+++
@@ -1,4 +0,0 @@
-extends CanvasLayer
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/menus/titlemenu.tscn")
--- scripts/titlemenu.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
