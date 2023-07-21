99a98692818d7fdf1b52198d6373ebe09ddc4a35
Alexander <115094566+Agamess@users.noreply.github.com>
2023-07-21 11:03:53 +0200

    Delete resources directory

--- resources/enemy_resource.gd
+++
@@ -1,8 +0,0 @@
-class_name enemy_resource
-extends Resource
-
-@export var speed = 100
-@export var health = 100
-@export var damage = 20
-@export var size = Vector2(1,1)
-@export var color = Color(1,1,1)
