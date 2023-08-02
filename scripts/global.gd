5bdc14afebaa2db5b58de306a7966366e75d6967
themangomago <markus.schneider84@gmail.com>
2023-08-02 20:13:48 +0200

    added sound & music config; added config saving/loading

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,6 +7,38 @@ enum {
 var enemies_in_range : Dictionary
 var state = WANDER
 
+
+
+var config: Dictionary = {
+	"music": true,
+	"sound": true
+}
+
+func _ready():
+	load_config()
+	set_music(config.music)
+	set_sound(config.sound)
+
+func load_config():
+	if not FileAccess.file_exists("user://config.cfg"):
+		save_config()
+		return
+	var file := FileAccess.open("user://config.cfg", FileAccess.READ)
+	config = JSON.new().parse_string(file.get_as_text())
+
+
+func save_config():
+	var file := FileAccess.open("user://config.cfg", FileAccess.WRITE)
+	file.store_string(JSON.stringify(config, "\t"))
+
+func set_music(target):
+	config.music = target
+	AudioServer.set_bus_mute(1, !target)
+
+func set_sound(target):
+	config.sound = target
+	AudioServer.set_bus_mute(2, !target)
+
 func _process(delta):
 	if true in enemies_in_range.values():
 		state = FOLLOW
--- scripts/titlemenu.gd
+++ scripts/titlemenu.gd
@@ -1,8 +1,31 @@
 extends Control
 
+
+func _ready():
+	if Global.config.music:
+		$MusicSwitch/CheckButton.button_pressed = true
+	if Global.config.sound:
+		$SoundSwitch/CheckButton.button_pressed = true
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 
 func _on_shooter_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/shooter_level_1.tscn")
+
+
+func _on_dont_click_me_button_up():
+	print("Please dont poke the baby")
+	$DontClickMe/AudioStreamPlayer2D.play()
+
+
+func _on_music_button_button_up():
+	Global.set_music($MusicSwitch/CheckButton.button_pressed)
+	Global.save_config()
+
+
+
+func _on_sound_button_button_up():
+	Global.set_sound($SoundSwitch/CheckButton.button_pressed)
+	Global.save_config()
