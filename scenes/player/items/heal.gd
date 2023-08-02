a79d6b6f269f1720cdc1f1a14ca6ed389e5de821
themangomago <markus.schneider84@gmail.com>
2023-08-02 16:34:10 +0200

    added a cool looking medikit

--- scenes/player/items/heal.gd
+++ scenes/player/items/heal.gd
@@ -5,4 +5,9 @@ extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("family"):
 		body.get_hit(-50)
+		$AnimationPlayer.play("pickup")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "pickup":
 		queue_free()
